01bb7c4f68cdc46b6acde56996924aae5c2824b9
Bruno Vilefort <bruno.clara@yahoo.com>
2022-08-21 23:36:46 -0300

    feat: add store api project

---
+++ backend/Store API/app.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+require("express-async-errors");
+
+/* Async errors */
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+/* Middlewares */
+app.use(express.json());
+
+/* Routes */
+app.get("/", (req, res) => {
+  res.send("<h1>Store API</h1><a href='/api/v1/products'>products route</a>");
+});
+
+app.use("/api/v1/products", productsRouter);
+
+/* Products route */
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+/* ConnectDB */
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
