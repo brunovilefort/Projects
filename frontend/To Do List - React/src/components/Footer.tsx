938ce8045c22d3d474b9617645a26b0332757afe
Bruno Vilefort <bruno.clara@yahoo.com>
2022-08-23 12:55:56 -0300

    feat: add project to do list with react and typescript

---
+++ frontend/To Do List - React/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// CSS:
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span>React + TS ToDo</span> @ 2022
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
